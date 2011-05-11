5f254e564d8c1840cce54e234d2771dc8d6fc98c
NumberFour8 <lukas.pohanka@inina.net>
2011-05-11 22:48:22 +0200

    Prvni commit

---
+++ HashCracker/MainForm.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace HashCracker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textCharset = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericLength = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboAlgo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textHash = new System.Windows.Forms.TextBox();
+            this.labelHash = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.labelTimeRemaining = new System.Windows.Forms.Label();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.labelRemainingCombinations = new System.Windows.Forms.Label();
+            this.labelTotalCombinations = new System.Windows.Forms.Label();
+            this.progressTotal = new System.Windows.Forms.ProgressBar();
+            this.buttonStartStop = new System.Windows.Forms.Button();
+            this.timerUpdater = new System.Windows.Forms.Timer(this.components);
+            this.labelResult = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericLength)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textCharset);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numericLength);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboAlgo);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textHash);
+            this.groupBox1.Controls.Add(this.labelHash);
+            this.groupBox1.Location = new System.Drawing.Point(2, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 136);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Zadání";
+            // 
+            // textCharset
+            // 
+            this.textCharset.Location = new System.Drawing.Point(103, 99);
+            this.textCharset.Name = "textCharset";
+            this.textCharset.Size = new System.Drawing.Size(182, 20);
+            this.textCharset.TabIndex = 7;
+            this.textCharset.Text = "abcdefghijklmnopqrstuvwxyz1234567890";
+            this.textCharset.Validating += new System.ComponentModel.CancelEventHandler(this.textCharset_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Znaková sada :";
+            // 
+            // numericLength
+            // 
+            this.numericLength.Location = new System.Drawing.Point(103, 73);
+            this.numericLength.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericLength.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericLength.Name = "numericLength";
+            this.numericLength.Size = new System.Drawing.Size(56, 20);
+            this.numericLength.TabIndex = 5;
+            this.numericLength.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Maximální délka :";
+            // 
+            // comboAlgo
+            // 
+            this.comboAlgo.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboAlgo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboAlgo.FormattingEnabled = true;
+            this.comboAlgo.Items.AddRange(new object[] {
+            "MD5",
+            "SHA256"});
+            this.comboAlgo.Location = new System.Drawing.Point(103, 45);
+            this.comboAlgo.Name = "comboAlgo";
+            this.comboAlgo.Size = new System.Drawing.Size(121, 21);
+            this.comboAlgo.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Druh algoritmu :";
+            // 
+            // textHash
+            // 
+            this.textHash.Location = new System.Drawing.Point(51, 17);
+            this.textHash.Name = "textHash";
+            this.textHash.Size = new System.Drawing.Size(234, 20);
+            this.textHash.TabIndex = 1;
+            // 
+            // labelHash
+            // 
+            this.labelHash.AutoSize = true;
+            this.labelHash.Location = new System.Drawing.Point(6, 20);
+            this.labelHash.Name = "labelHash";
+            this.labelHash.Size = new System.Drawing.Size(38, 13);
+            this.labelHash.TabIndex = 0;
+            this.labelHash.Text = "Hash :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labelTimeRemaining);
+            this.groupBox2.Controls.Add(this.labelSpeed);
+            this.groupBox2.Controls.Add(this.labelRemainingCombinations);
+            this.groupBox2.Controls.Add(this.labelTotalCombinations);
+            this.groupBox2.Controls.Add(this.progressTotal);
+            this.groupBox2.Location = new System.Drawing.Point(2, 145);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(291, 106);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Průběh";
+            // 
+            // labelTimeRemaining
+            // 
+            this.labelTimeRemaining.AutoSize = true;
+            this.labelTimeRemaining.Location = new System.Drawing.Point(160, 56);
+            this.labelTimeRemaining.Name = "labelTimeRemaining";
+            this.labelTimeRemaining.Size = new System.Drawing.Size(77, 13);
+            this.labelTimeRemaining.TabIndex = 4;
+            this.labelTimeRemaining.Text = "Zbývá (minut) :";
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Location = new System.Drawing.Point(6, 57);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(54, 13);
+            this.labelSpeed.TabIndex = 3;
+            this.labelSpeed.Text = "Rychlost :";
+            // 
+            // labelRemainingCombinations
+            // 
+            this.labelRemainingCombinations.AutoSize = true;
+            this.labelRemainingCombinations.Location = new System.Drawing.Point(6, 16);
+            this.labelRemainingCombinations.Name = "labelRemainingCombinations";
+            this.labelRemainingCombinations.Size = new System.Drawing.Size(94, 13);
+            this.labelRemainingCombinations.TabIndex = 2;
+            this.labelRemainingCombinations.Text = "Zbývá vyzkoušet :";
+            // 
+            // labelTotalCombinations
+            // 
+            this.labelTotalCombinations.AutoSize = true;
+            this.labelTotalCombinations.Location = new System.Drawing.Point(6, 34);
+            this.labelTotalCombinations.Name = "labelTotalCombinations";
+            this.labelTotalCombinations.Size = new System.Drawing.Size(134, 13);
+            this.labelTotalCombinations.TabIndex = 1;
+            this.labelTotalCombinations.Text = "Celkový počet kombinací :";
+            // 
+            // progressTotal
+            // 
+            this.progressTotal.Location = new System.Drawing.Point(6, 77);
+            this.progressTotal.Name = "progressTotal";
+            this.progressTotal.Size = new System.Drawing.Size(279, 23);
+            this.progressTotal.TabIndex = 0;
+            // 
+            // buttonStartStop
+            // 
+            this.buttonStartStop.Location = new System.Drawing.Point(2, 257);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartStop.TabIndex = 2;
+            this.buttonStartStop.Text = "Spustit";
+            this.buttonStartStop.UseVisualStyleBackColor = true;
+            this.buttonStartStop.Click += new System.EventHandler(this.buttonStartStop_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(95, 262);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(66, 13);
+            this.labelResult.TabIndex = 3;
+            this.labelResult.Text = "Výsledek: ";
+            this.labelResult.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 283);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonStartStop);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Prolamovač hashí";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericLength)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericLength;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboAlgo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textHash;
+        private System.Windows.Forms.Label labelHash;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label labelTimeRemaining;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.Label labelRemainingCombinations;
+        private System.Windows.Forms.Label labelTotalCombinations;
+        private System.Windows.Forms.ProgressBar progressTotal;
+        private System.Windows.Forms.Button buttonStartStop;
+        private System.Windows.Forms.TextBox textCharset;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timerUpdater;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ HashCracker/MainForm.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCracker
+{
+    public partial class MainForm : Form
+    {
+        private Thread CrackingThread; // Vlákno ve kterém poběží prolamování
+        private delegate string HashFunction(string Input); // Delegát pro obecnou hashovací funkci
+        private string FoundPassword; // Nalezené heslo
+        private UInt64 Progress;
+
+        private object LockObject;
+
+        /// <summary>
+        /// Počet všech kombinací pro vyzkoušení podle zadaných údajů ve formuláři
+        /// </summary>
+        public UInt64 Combinations
+        {
+            get
+            {
+                return (UInt64)Math.Pow((double)textCharset.Text.Length, (double)numericLength.Value);
+            }
+        }
+
+        /// <summary>
+        /// Struktura s informacemi ke crackování předávaná do vlákna
+        /// </summary>
+        private struct CrackerInfo
+        {
+            public string Hash, Charset;
+            public int MaxLength;
+            public HashFunction F;
+        }
+
+        /// <summary>
+        /// Výchozí konstruktor formuláře
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            CrackingThread = new Thread(new ParameterizedThreadStart(Cracker));
+            LockObject = new object();
+
+            FoundPassword = null;
+        }
+
+        /// <summary>
+        /// Převede zadané číslo do číselné soustavy o daném základu
+        /// </summary>
+        /// <param name="Number">Číslo</param>
+        /// <param name="Base">Základ nové číselné soustavy</param>
+        /// <returns>Cifry čísla v dané číselné soustavě</returns>
+        private int[] ConvertToBase(UInt64 Number, int Base)
+        {
+            if (Base >= 1)
+                throw new ArgumentException();
+
+            List<int> Ret = new List<int>();
+            
+            if (Base == 10)
+                return Number.ToString().ToCharArray().Cast<int>().ToArray();
+
+            int digit = 0;
+            while (Number > 0)
+            {
+                digit = (int)(Number % (ulong)Base);
+                Ret.Add(digit);
+                Number = (Number-(ulong)digit)/(ulong)Base;
+            }
+
+            Ret.Reverse();
+            return Ret.ToArray();
+        }
+
+
+        /// <summary>
+        /// Metoda bežící ve vlákně
+        /// </summary>
+        /// <param name="inf">Informace předávané dovnitř vlákna</param>
+        private void Cracker(object inf)
+        {
+            CrackerInfo Info = (CrackerInfo)inf;
+            
+            short CharsetLength = (short)Info.Charset.Length;
+            int MaximumLength = Info.MaxLength;
+            UInt64 Combinations = (UInt64)Math.Pow((double)CharsetLength, (double)MaximumLength);
+
+            string Test = String.Empty;
+            for (Progress = 0; Progress < Combinations; Progress++)
+            {
+                lock (LockObject)
+                {
+                    Test = String.Empty;
+                    int[] Digits = ConvertToBase(Progress, CharsetLength);
+                    for (int j = 0; j < Digits.Length; j++)
+                       Test += Info.Charset[Digits[j]];
+                    
+                    if (Info.Hash.ToLower() == Info.F(Test).ToLower())
+                    {
+                        FoundPassword = Test;
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Převede bajty dat do hexadecimální notace
+        /// </summary>
+        /// <param name="Data">Data pro převod</param>
+        /// <returns>Data v hexadecimální textové notaci</returns>
+        private string ConvertHexToString(byte[] Data)
+        {
+            string HexString = String.Empty;
+            foreach (byte b in Data)
+                HexString += Convert.ToString(b, 16);
+            return HexString;
+        }
+
+        /// <summary>
+        /// Spočítá MD5 hashi ze vstupního řetězce
+        /// </summary>
+        /// <param name="In">Vstupní řetězec</param>
+        /// <returns>MD5 hashe v hexadecimální notaci</returns>
+        private string MD5Hash(string In)
+        {
+            using (MD5 Hash = MD5.Create())
+            {
+                Hash.Initialize();
+                byte[] OutHash = Hash.ComputeHash(Encoding.ASCII.GetBytes(In));
+
+                return ConvertHexToString(OutHash);
+            }
+        }
+
+        /// <summary>
+        /// Spočítá SHA256 hashi ze vstupního řetězce
+        /// </summary>
+        /// <param name="In">Vstupní řetězec</param>
+        /// <returns>SHA256 hashe v hexadecimální notaci</returns>
+        private string SHA256Hash(string In)
+        {
+            using (SHA256 Hash = SHA256.Create())
+            {
+                Hash.Initialize();
+                byte[] OutHash = Hash.ComputeHash(Encoding.ASCII.GetBytes(In));
+
+                return ConvertHexToString(OutHash);
+            }
+        }
+
+
+        #region Události
+
+        private void buttonStartStop_Click(object sender, EventArgs e)
+        {
+            if (CrackingThread.ThreadState == ThreadState.Unstarted || CrackingThread.ThreadState == ThreadState.Stopped)
+            {
+                if (textHash.Text.Length < 32)
+                {
+                    MessageBox.Show("Byl zadán neplatný hash");
+                    return;
+                }
+
+                CrackerInfo Info = new CrackerInfo();
+                Info.Hash = textHash.Text;
+                Info.Charset = textCharset.Text;
+                Info.MaxLength = (int)numericLength.Value;                
+
+                CrackingThread.Start();
+            
+                buttonStartStop.Text = "Pozastavit";
+            }
+            else if (CrackingThread.ThreadState == ThreadState.Running)
+            {
+                Monitor.Enter(LockObject);
+                buttonStartStop.Text = "Obnovit";
+            }
+            else if (CrackingThread.ThreadState == ThreadState.WaitSleepJoin)
+            {
+                Monitor.Exit(LockObject);
+                buttonStartStop.Text = "Pozastavit";
+            }
+        }
+
+        private void textCharset_Validating(object sender, CancelEventArgs e)
+        {
+            char[] chars = textCharset.Text.ToCharArray();
+
+            int count = chars.Distinct().Count();
+
+            if (count != chars.Length) e.Cancel = true;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (CrackingThread.ThreadState == ThreadState.Running)
+            {
+                CrackingThread.Abort();
+            }
+            else if (CrackingThread.ThreadState == ThreadState.WaitSleepJoin)
+            {
+                try
+                {
+                    Monitor.Exit(LockObject);
+                    CrackingThread.Abort();
+                }
+                catch (SynchronizationLockException)
+                {
+                    // Tady nic moc nenaděláme, vlákno stojí z neznámého důvodu
+                    CrackingThread.Abort();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
